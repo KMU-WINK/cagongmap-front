cfb9e7c6b1d66586318a4a7d735b48a75e9d242c
Pyosangwoo <psw3619@naver.com>
2021-10-07 14:45:38 +0900

    add: add button img

--- src/component/MainPage/SelectMenu.js
+++ src/component/MainPage/SelectMenu.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import img_menu1 from './Images/img_menu1.png';
+import img_menu2 from './Images/img_menu2.png';
+import img_search from './Images/img_search.png';
+
 export const SelectMenu = () => {
+    const btnStyle = {
+        
+      };
+
+    
     return <>
-        <div>SelectMenu입니다</div>
+
+    <div>
+        <button style = {btnStyle}> <img src ={img_menu1} alt = "img_menu1"></img></button>
+        <button> <img src ={img_menu2} alt = "img_menu2"></img> </button>
+        <button> <img src ={img_search} alt = "img_search"></img></button>
+    </div>
     </>
 }
+
+export default SelectMenu;
