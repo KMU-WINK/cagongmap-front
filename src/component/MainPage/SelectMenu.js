e1e0110777fcf6166e22ed5efb4736ba3ce68391
Pyosangwoo <psw3619@naver.com>
2021-10-07 21:02:38 +0900

    add: button location

--- src/component/MainPage/SelectMenu.js
+++ src/component/MainPage/SelectMenu.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React from "react";
 import img_menu1 from './Images/img_menu1.png';
 import img_menu2 from './Images/img_menu2.png';
 import img_search from './Images/img_search.png';
 
+
 export const SelectMenu = () => {
-    const btnStyle = {
-        
+
+    const parentStyle = {
+        position: "absolute",
+        left: "8%",
+        bottom: "15px",
+        color: "white",
+        padding: "10px",
+        width: "80%",
+        height: "15%",
+        backgroundColor: "gray"
       };
 
-    
-    return <>
+      const styleFirst = {
+        float: "left",
+        width: "33%",
+        height: "100%",
+        padding: "10px 0px",
+        backgroundColor: "rgba(255, 255, 255, 0.05)"
+      };
+      const styleSecond = {
+        float: "left",
+        width: "33%",
+        height: "100%",
+        padding: "10px 0px",
+        backgroundColor: "green",
+      };
+      const styleThird = {
+        float: "left",
+        width: "33%",
+        height: "100%",
+        padding: "10px 0px",
+        backgroundColor: "red"
+      };
 
+    return <>
+    
     <div>
-        <button style = {btnStyle}> <img src ={img_menu1} alt = "img_menu1"></img></button>
-        <button> <img src ={img_menu2} alt = "img_menu2"></img> </button>
-        <button> <img src ={img_search} alt = "img_search"></img></button>
+        <div style={parentStyle}>
+            <button style={styleFirst}> <img src ={img_menu1} alt = "table" width="50%" height="90%" /> <br/> 테이블 </button>
+            <button style={styleSecond}> <img src ={img_menu2} alt = "outlet" width="50%" height="90%" /> <br/> 콘센트 </button>
+            <button style={styleThird}> <img src ={img_search} alt = "search" width="50%" height="90%" /> </button>
+        </div>
     </div>
+ 
+    
+
     </>
 }
 
