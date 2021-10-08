c7538ca726cf73ca95ee1e1c068ec43c57402483
이예원 <81704418+scarletYewon@users.noreply.github.com>
2021-10-09 00:46:01 +0900

    SplashPage

--- src/page/SplashPage.js
+++ src/page/SplashPage.js
@@ -1,5 +1,14 @@
+import React from "react";
+import Group1 from '../img/Group1.png';
+import './SplashPage.css';
 export const SplashPage = () => {
+
     return <>
-        <div>SplashPage입니다</div>
+        <div className="SplashPage">
+            <img className="logoimg" alt="logo" src={Group1} />
+            <div className="SplashPageText">
+                서비스 이용을 원하신다면 화면을 클릭해주세요
+            </div>
+        </div>
     </>
 }
