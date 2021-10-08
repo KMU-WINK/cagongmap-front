14de00e5e8978f62059ad6c9eacef5c610653b18
Hyewon23 <ch2w2006@kookmin.ac.kr>
2021-10-09 06:54:21 +0900

    [MainPage]search category

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -8,12 +8,15 @@ import img_focus_green from "../svg/img_focus_green.svg";
 
 /*global kakao*/
 const markers = []
+const result1 = []
+const result2 = []
 let map;
 
 export const MainPage = () => {
     const [plug, setPlug] = useState(false);
     const [focus, setFocus] = useState(false);
     let ps;
+    let infowindow = new kakao.maps.InfoWindow({zIndex:1});
 
     useEffect(()=>{
         myLocate();
@@ -29,23 +32,22 @@ export const MainPage = () => {
         };
 
         // 현재 위치
-        if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(function(position){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
                 let lat = position.coords.latitude,
                     lon = position.coords.longitude;
 
                 let locPosition = new kakao.maps.LatLng(lat, lon)
                 displayMyLocate(locPosition);
             });
-        }
-        else {
-            let locPosition = new kakao.maps.LatLng(37.5677463677699,126.9153946742084)
+        } else {
+            let locPosition = new kakao.maps.LatLng(37.5677463677699, 126.9153946742084)
             displayMyLocate(locPosition);
         }
 
         map = new kakao.maps.Map(container, options);
 
-        function displayMyLocate(locPosition){
+        function displayMyLocate(locPosition) {
             // 마커를 생성합니다.
             let marker = new kakao.maps.Marker({
                 map: map,
@@ -56,17 +58,17 @@ export const MainPage = () => {
         }
 
         // 지도를 클릭했을때 클릭한 위치에 마커를 추가하도록 지도에 클릭이벤트를 등록합니다
-        kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+        kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
             // 클릭한 위치에 마커를 표시합니다
             addMarker(mouseEvent.latLng);
         });
 
-    // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
+        // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
 
-    // // 마커 하나를 지도위에 표시합니다
-    //     addMarker(new kakao.maps.LatLng(37.610189448398906, 126.99703140609459));
+        // // 마커 하나를 지도위에 표시합니다
+        //     addMarker(new kakao.maps.LatLng(37.610189448398906, 126.99703140609459));
 
-    // 마커를 생성하고 지도위에 표시하는 함수입니다
+        // 마커를 생성하고 지도위에 표시하는 함수입니다
         function addMarker(position) {
 
             // 마커를 생성합니다
@@ -80,6 +82,8 @@ export const MainPage = () => {
             // 생성된 마커를 배열에 추가합니다
             markers.push(marker);
         }
+
+
     }
 
     const clickFocus = () => {
@@ -90,10 +94,40 @@ export const MainPage = () => {
     const clickPlug = () => {
         setPlug(!plug)
         console.log(plug)
+
         console.log(markers)
-        for (let i = 0; i < markers.length; i++) {
-            if (plug) markers[i].setMap(map);
-            else markers[i].setMap(null);
+
+        let ps = new kakao.maps.services.Places(map);
+
+        // 카테고리로 은행을 검색합니다
+       ps.categorySearch('CE7', placesSearchCB, {useMapBounds: true});
+
+        // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+        function placesSearchCB(data, status, pagination) {
+            if (status === kakao.maps.services.Status.OK) {
+                for (let i = 0; i < data.length; i++) {
+                    if (plug) displayMarker(data[i]);
+                    else displayMarker(data[i])
+                }
+            }
+        }
+
+        console.log(result1)
+
+        function displayMarker(place) {
+            // 마커를 생성하고 지도에 표시합니다
+            let marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(place.y, place.x)
+            });
+
+            result1.push(marker)
+            // 마커에 클릭이벤트를 등록합니다
+            kakao.maps.event.addListener(marker, 'click', function () {
+                // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+                infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+                infowindow.open(map, marker);
+            });
         }
     }
 
