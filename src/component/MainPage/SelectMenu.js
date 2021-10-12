4c8549eb034df6e0c02349f2b96f41062af129bf
Pyosangwoo <psw3619@naver.com>
2021-10-12 15:34:35 +0900

    Modify: add label

--- src/component/MainPage/SelectMenu.js
+++ src/component/MainPage/SelectMenu.js
@@ -8,11 +8,17 @@ export const SelectMenu = () => {
 
     return <>
         <div className="container_menu">
-            <button className="table"> <img src ={img_menu1} alt = "table"/> <br/> 테이블 </button>
-            <button className="outlet"> <img src ={img_menu2} alt = "outlet"/> <br/> 콘센트 </button>
-            <button className="search"> <img src ={img_search} alt = "search"/> </button>
-        </div>        
+            <button className="table" id = "table">
+                <img className="img_table" src ={img_menu1} alt = "table"/> <br/><br/>
+                <label className="label_table" for="table"> 테이블: 선택 안함 </label>
+            </button>
+            <button className="outlet" id = "outlet">
+                <img className="img_outlet" src ={img_menu2} alt = "outlet"/> <br/><br/>
+                <label className="label_outlet" for="outlet"> 콘센트: 선택 안함 </label>
+            </button>
+            <button className="search">
+                <img className="img_search" src ={img_search} alt = "search"/>
+            </button>
+        </div>
     </>
 }
-
-export default SelectMenu;
