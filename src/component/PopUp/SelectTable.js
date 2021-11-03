d51002c2d2daaf9be95e9b19f7ee23dee527fb5d
Pyosangwoo <psw3619@naver.com>
2021-11-03 23:15:43 +0900

    add: tablepopup add

--- src/component/PopUp/SelectTable.js
+++ src/component/PopUp/SelectTable.js
@@ -1,8 +1,26 @@
 import styled from "styled-components";
+import './SelectTable.css';
+import img_menu1 from "../../img/img_menu1.png";
 
 export const SelectTable = () => {
     return <>
-        <Text>table</Text>
+       
+          <button className="singletable">
+            <img className="img_singletable" src={img_menu1} alt="table"/>
+            <label className="label_singletable" htmlFor="table"> 싱글 테이블(1~2인) </label>
+          </button>
+          <button className="doubletable">
+            <img className="img_singletable" src={img_menu1} alt="table"/>
+            <label className="label_singletable" htmlFor="table"> 더블테이블(1~4인) </label>
+          </button>
+          <button className="bartable">
+            <img className="img_singletable" src={img_menu1} alt="table"/>
+            <label className="label_singletable" htmlFor="table"> 바 테이블(n인) </label>
+          </button>
+          <button className="anything">
+            <img className="img_singletable" src={img_menu1} alt="table"/>
+            <label className="label_singletable" htmlFor="table"> 상관 없어요! </label>
+          </button>
     </>
 }
 
