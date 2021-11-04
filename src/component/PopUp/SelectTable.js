7b7f412b9ffb456d6e1e9ea1efbc4431fff59d25
Pyosangwoo <psw3619@naver.com>
2021-11-04 23:30:22 +0900

    Update SelectTable.js

--- src/component/PopUp/SelectTable.js
+++ src/component/PopUp/SelectTable.js
@@ -11,20 +11,20 @@ export const SelectTable = () => {
   }
   
     return <>
-          <button className="singletable" id="singletable" onClick={() => setSelect('singletable')}>
+          <button className="singletable" id="singletable" onClick={() => getState('singletable')}>
             <img className="img_singletable" src={img_menu1} alt="singletable"/>
             <label className="label_singletable" htmlFor="table"> 싱글 테이블(1~2인) </label>
           </button>
-          <button className="doubletable" id="doubletable" onClick={() => setSelect('doubletable')}>
+          <button className="doubletable" id="doubletable" onClick={() => getState('doubletable')}>
             <img className="img_doubletable1" src={img_menu1} alt="doubletable"/>
             <img className="img_doubletable2" src={img_menu1} alt="doubletable"/>
             <label className="label_doubletable" htmlFor="table"> 더블테이블(1~4인) </label>
           </button>
-          <button className="bartable" id="bartable" onClick={() => setSelect('bartable')}>
+          <button className="bartable" id="bartable" onClick={() => getState('bartable')}>
             <img className="img_bartable" src={img_bartable} alt="bartable"/>
             <label className="label_bartable" htmlFor="table"> 바 테이블(n인) </label>
           </button>
-          <button className="anything" id="bartable" onClick={() => setSelect('anything')}>
+          <button className="anything" id="bartable" onClick={() => getState('anything')}>
             <img className="img_anything" src={img_anything} alt="anything"/>
             <label className="label_anything" htmlFor="table"> 상관 없어요! </label>
           </button>
