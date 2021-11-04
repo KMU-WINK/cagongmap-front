25abb27b205096a62ba2462d9c58eebbdee9a022
Pyosangwoo <psw3619@naver.com>
2021-11-04 23:48:08 +0900

    Modify: button click border color change

--- src/component/PopUp/SelectTable.js
+++ src/component/PopUp/SelectTable.js
@@ -1,54 +1,55 @@
 import './SelectTable.css';
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import img_menu1 from "../../img/img_menu1.png";
 import img_bartable from "../../img/img_bartable.png";
 import img_anything from "../../img/img_anything.png";
 
 export const SelectTable = () => {
-  const [select, setSelect] = useState('')
-  const getState = (state) => {
-    setSelect(state);
-  }
-  
+  const [currentClick, setCurrentClick] = useState(null);
+  const [prevClick, setPrevClick] = useState(null);
+
+  const GetClick = (e) => {
+  setCurrentClick(e);
+  console.log(e);
+  };
+
+  useEffect(
+    (e) => {
+      if (currentClick !== null) {
+        let current = document.getElementById(currentClick);
+        console.log(current);
+        current.style.border = "3px solid #4AD395"
+      }
+
+      if (prevClick !== null) {
+        let prev = document.getElementById(prevClick);
+        prev.style.border = "3px solid #C4C4C4";
+        
+      }
+      setPrevClick(currentClick);
+    },
+    [currentClick]
+  );
+
     return <>
-          <button className="singletable" id="singletable" onClick={() => getState('singletable')}>
+          <button className="singletable" id="singletable" onClick={() => GetClick('singletable')}>
             <img className="img_singletable" src={img_menu1} alt="singletable"/>
             <label className="label_singletable" htmlFor="table"> 싱글 테이블(1~2인) </label>
           </button>
-          <button className="doubletable" id="doubletable" onClick={() => getState('doubletable')}>
+          <button className="doubletable" id="doubletable" onClick={() => GetClick('doubletable')}>
             <img className="img_doubletable1" src={img_menu1} alt="doubletable"/>
             <img className="img_doubletable2" src={img_menu1} alt="doubletable"/>
             <label className="label_doubletable" htmlFor="table"> 더블테이블(1~4인) </label>
           </button>
-          <button className="bartable" id="bartable" onClick={() => getState('bartable')}>
+          <button className="bartable" id="bartable" onClick={() => GetClick('bartable')}>
             <img className="img_bartable" src={img_bartable} alt="bartable"/>
             <label className="label_bartable" htmlFor="table"> 바 테이블(n인) </label>
           </button>
-          <button className="anything" id="bartable" onClick={() => getState('anything')}>
+          <button className="anything" id="anything" onClick={() => GetClick('anything')}>
             <img className="img_anything" src={img_anything} alt="anything"/>
             <label className="label_anything" htmlFor="table"> 상관 없어요! </label>
           </button>
-
-          {select === 'singletable'?
-            console.log('single')
-            :
-            <>
-            {select === 'doubletable'?
-                console.log('double')
-                :
-                <>
-                {select === 'bartable'?
-                console.log('bartable')
-                :
-                <>
-                  {select === 'anything'?
-                  console.log('anything')
-                  :
-                  null
-                  }</>
-                }</>
-            }</>
-          }
+          
     </>
 }
 
