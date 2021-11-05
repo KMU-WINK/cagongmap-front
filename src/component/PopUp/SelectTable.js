3c99a326a3967fb768778392da416493670d2e4f
Pyosangwoo <psw3619@naver.com>
2021-11-05 20:45:59 +0900

    Modify: label color change

--- src/component/PopUp/SelectTable.js
+++ src/component/PopUp/SelectTable.js
@@ -10,21 +10,22 @@ export const SelectTable = () => {
 
   const GetClick = (e) => {
   setCurrentClick(e);
-  console.log(e);
   };
 
   useEffect(
     (e) => {
       if (currentClick !== null) {
-        let current = document.getElementById(currentClick);
-        console.log(current);
-        current.style.border = "3px solid #4AD395"
-      }
+        let currentbtn = document.querySelector(`#${currentClick}`);
+        let currentlabel = document.querySelector(`label[id=${currentClick}]`);
+        currentbtn.style.border = "3px solid #4AD395";
+        currentlabel.style.color = "#4AD395";
+       }
 
       if (prevClick !== null) {
-        let prev = document.getElementById(prevClick);
-        prev.style.border = "3px solid #C4C4C4";
-        
+        let prevbtn = document.querySelector(`#${prevClick}`);
+        let prevlabel = document.querySelector(`label[id=${prevClick}]`)
+        prevbtn.style.border = "3px solid #C4C4C4";
+        prevlabel.style.color = "#000000"
       }
       setPrevClick(currentClick);
     },
@@ -34,20 +35,20 @@ export const SelectTable = () => {
     return <>
           <button className="singletable" id="singletable" onClick={() => GetClick('singletable')}>
             <img className="img_singletable" src={img_menu1} alt="singletable"/>
-            <label className="label_singletable" htmlFor="table"> 싱글 테이블(1~2인) </label>
+            <label className="label_singletable" id="singletable" htmlFor="table"> 싱글 테이블(1~2인) </label>
           </button>
           <button className="doubletable" id="doubletable" onClick={() => GetClick('doubletable')}>
             <img className="img_doubletable1" src={img_menu1} alt="doubletable"/>
             <img className="img_doubletable2" src={img_menu1} alt="doubletable"/>
-            <label className="label_doubletable" htmlFor="table"> 더블테이블(1~4인) </label>
+            <label className="label_doubletable" id="doubletable" htmlFor="table"> 더블테이블(1~4인) </label>
           </button>
           <button className="bartable" id="bartable" onClick={() => GetClick('bartable')}>
             <img className="img_bartable" src={img_bartable} alt="bartable"/>
-            <label className="label_bartable" htmlFor="table"> 바 테이블(n인) </label>
+            <label className="label_bartable" id="bartable" htmlFor="table"> 바 테이블(n인) </label>
           </button>
           <button className="anything" id="anything" onClick={() => GetClick('anything')}>
             <img className="img_anything" src={img_anything} alt="anything"/>
-            <label className="label_anything" htmlFor="table"> 상관 없어요! </label>
+            <label className="label_anything" id="anything" htmlFor="table"> 상관 없어요! </label>
           </button>
           
     </>
