c6925963edd975cc61f3a7cc05b2288ee64f6df2
Pyosangwoo <psw3619@naver.com>
2021-11-16 22:02:43 +0900

    Add: BackBtn, Header

--- src/component/Detail/BackBtn.js
+++ src/component/Detail/BackBtn.js
@@ -1,8 +1,32 @@
 import styled from "styled-components";
+import React, {useEffect, useState} from 'react';
+import {useHistory} from "react-router-dom";
+import left_arrow from "../../img/left_arrow.png";
 
 export const BackBtn = () => {
-
+    const history = useHistory();
     return <>
-
+        <BackButton onClick={()=>{history.push('/main')}}>
+            <BackBtn_img/>
+        </BackButton>
     </>
 }
+
+const BackButton = styled.div`
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    left: 20px;
+    top: 20px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #FFFFFF;
+`
+const BackBtn_img = styled.img.attrs({
+    src : left_arrow
+})`
+    width: 20px;
+    height: 20px;
+`
